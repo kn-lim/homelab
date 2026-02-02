8b8475390e34f25eed1ab93fb6dfa11c73e59067
Kevin Lim <knlim95@gmail.com>
2026-02-01 22:17:44 -0800

    chore: rename to default

--- terraform/_modules/onepassword-secret/outputs.tf
+++ terraform/_modules/onepassword-secret/outputs.tf
@@ -2,12 +2,12 @@ output "fields" {
   description = "All fields from the 1Password item as a map"
   value = merge(
     {
-      credential = data.onepassword_item.this.credential
-      username   = data.onepassword_item.this.username
-      password   = data.onepassword_item.this.password
-      note_value = data.onepassword_item.this.note_value
+      credential = data.onepassword_item.default.credential
+      username   = data.onepassword_item.default.username
+      password   = data.onepassword_item.default.password
+      note_value = data.onepassword_item.default.note_value
     },
-    { for field in flatten([for section in data.onepassword_item.this.section : section.field]) : field.label => field.value }
+    { for field in flatten([for section in data.onepassword_item.default.section : section.field]) : field.label => field.value }
   )
   sensitive = true
 }
